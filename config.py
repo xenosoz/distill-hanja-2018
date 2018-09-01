ef82595f56fc95ea4820e530541f95458419b431
Taihyun Hwang <xenosoz.hwang@gmail.com>
2018-09-01 23:37:26 +0900

    extract properties from the wiki template

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ dumpbz2 = 'objects/kowiktionary-20180820-pages-articles-multistream.xml.bz2'
 
 raw_hanja = 'objects/raw-hanja.txt'
 
+props_hanja = 'objects/props-hanja.txt'
--- driver.py
+++ driver.py
@@ -49,9 +49,51 @@ def extract_raw_hanja(src=config.dumpbz2, dest=Path(config.raw_hanja)):
                     print(repr(x), file=file)
 
 
+def extract_props(src=config.raw_hanja, dest=Path(config.props_hanja)):
+    if dest.exists():
+        print('exists: {}'.format(dest))
+        return
+    else:
+        print('extract: {}'.format(dest))
+
+        
+    from ast import literal_eval
+
+    with open(src) as ifile, open(dest, 'w') as ofile:
+        for line in ifile:
+            line = line.rstrip()
+            character, template = literal_eval(line)
+
+            props = dict()            
+            for item in template.replace('{{', '').replace('}}', '').split('|'):
+                kv = item.split('=')
+                if len(kv) == 2:
+                    key, value = kv
+                    value = value.replace('[[', '').replace(']]', '').replace('\t', '')
+                    value = value.replace('1. ', '').replace('2. ', '')
+                    value = value.split(',')
+                    value = [x.strip() for x in value]
+                    props[key] = value
+
+            for key in ['음2', '음3', '음4', '음5']:
+                props['음'] = props.get('음', []) + props.get(key, [])
+                props.pop(key, None)
+                
+            props['글자'] = character
+
+            if '신자체' in props or '간체' in props:
+                continue
+            
+            if not props.get('음'):
+                continue
+                
+            print(repr(props), file=ofile)
+
+
 def run():
     fetch()
     extract_raw_hanja()
+    extract_props()
 
     
 if __name__ == '__main__':
